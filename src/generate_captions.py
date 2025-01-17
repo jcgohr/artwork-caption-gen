9bf7ce1968df0f982482ae8f9b8a7099a461c038
James Gore <73591234+jgore077@users.noreply.github.com>
2025-01-17 16:52:54 -0500

    add tqdm

--- src/generate_captions.py
+++ src/generate_captions.py
@@ -1,5 +1,6 @@
 from parsers import CaptionGenerationParser  
-from captioners import * 
+from captioners import *
+from tqdm import tqdm 
 import json
 import os
 
@@ -18,7 +19,7 @@ output=args.output
 captions={}
 for class_name in confs:
     model=captioners[class_name](*confs[class_name])
-    for key in data:
+    for key in tqdm(data,desc=f"Generating captions with {class_name}"):
         if key not in captions:
             captions[key]={}
         captions[key][class_name]=model.caption(data[key][FP_KEY])
