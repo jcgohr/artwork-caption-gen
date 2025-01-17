27bdf88734d5fabed6c28e2fbf2d4e8418f2fbbf
James Gore <73591234+jgore077@users.noreply.github.com>
2025-01-17 14:10:13 -0500

    fix default config and build scaffold for generating captions

--- src/generate_captions.py
+++ src/generate_captions.py
@@ -13,6 +13,19 @@ Should we make a new caption file for every run of this script?
 
 """
 
+FP_KEY="file_path"
+captioners=globals()
 parser=CaptionGenerationParser()
 
+
 args=parser.parse_args()
+
+confs=args.config
+data=args.metadata
+output=args.output
+
+captions={}
+for class_name in confs:
+    model=captioners[class_name](*confs[class_name])
+    for key in data:
+        captions[key][class_name]=model.caption(data[key][FP_KEY])
