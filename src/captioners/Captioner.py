39c4eaabaad9bb1745c230e151d6e34f5662fdca
James Gore <73591234+jgore077@users.noreply.github.com>
2025-01-17 19:42:31 -0500

    add stub for multi gpu captioning

--- src/captioners/Captioner.py
+++ src/captioners/Captioner.py
@@ -1,4 +1,18 @@
+import torch
 
 class Captioner:
     def caption(self,image_path:str)->str:
         raise NotImplemented
+    
+    
+    
+def multi_gpu_captioning(captioners:list[str],params:list[list],data:dict):
+    if not torch.cuda.is_available():
+        raise EnvironmentError("CUDA is not available")
+    
+    gpus = torch.cuda.device_count()
+    if gpus==1:
+        raise ValueError("Only 1 GPU detected, try Captioner.sequential_captioning")
+    
+if __name__=="__main__":
+    multi_gpu_captioning([],[],{})
