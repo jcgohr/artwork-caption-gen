b8501bdf73c2bb1a087645fef57c687caf329a16
James Gore <73591234+jgore077@users.noreply.github.com>
2025-01-18 19:56:11 -0500

    closer to parallel captioning

--- src/captioners/Captioner.py
+++ src/captioners/Captioner.py
@@ -1,6 +1,8 @@
-from .utils import get_captioners
+from .utils import get_captioners,split_dict
+from multiprocessing import Pool
 from tqdm import tqdm
 import torch
+import json
 
 class Captioner:
     def caption(self,image_path:str)->str:
@@ -11,11 +13,11 @@ FP_KEY="file_path"
 
 
 
-def sequential_captioning(captioners:list[str],params:list[list],data:dict):
+def sequential_captioning(captioners:list[str],params:list[list],data:dict,device:str):
     captions={}
     classes=get_captioners()
     for captioner,param in zip(captioners,params):
-        model=classes[captioner](*param)
+        model=classes[captioner](*param,device=device)
         for key in tqdm(data,desc=f"Generating captions with {captioner}"):
             if key not in captions:
                 captions[key]={}
@@ -27,9 +29,18 @@ def multi_gpu_captioning(captioners:list[str],params:list[list],data:dict):
     if not torch.cuda.is_available():
         raise EnvironmentError("CUDA is not available")
     
-    gpus = torch.cuda.device_count()
+    # gpus = torch.cuda.device_count()
+    gpus=2
     if gpus==1:
         raise ValueError("Only 1 GPU detected, try Captioner.sequential_captioning instead")
     
-if __name__=="__main__":
-    multi_gpu_captioning([],[],{})
+    devices=[f"cuda:{i}" for i in range(gpus)]
+    subsets = split_dict(data,gpus)
+    for captioner,param in zip(captioners,params):
+        # Make variables compatible with sequential_captioning
+        captioner=[[captioner]]*gpus
+        param=[[param]]*gpus
+        map_params=[[a,b,c,d] for a,b,c,d in zip(captioner,param,subsets,devices)]
+        with Pool(gpus) as pool:
+            for result in pool.starmap(sequential_captioning,map_params):
+                print(result)
--- src/generate_captions.py
+++ src/generate_captions.py
@@ -1,26 +1,26 @@
 from parsers import CaptionGenerationParser 
 from captioners import *
 from tqdm import tqdm 
+import torch
 import json
 import os
 
 
-
-captioners=globals()
-parser=CaptionGenerationParser()
-
-
-args=parser.parse_args()
-
-confs=args.config
-data=args.metadata
-output=args.output
-
-captions={}
-params=list(confs.values())
-captioners=list(confs.keys())
+if __name__=="__main__":
+    captioners=globals()
+    parser=CaptionGenerationParser()
 
 
-captions=sequential_captioning(captioners,params,data)
-with open(output,"w",encoding="utf-8") as output_file:
-    output_file.write(json.dumps(captions,indent=4,ensure_ascii=False))
+    args=parser.parse_args()    
+
+    confs=args.config
+    data=args.metadata
+    output=args.output
+
+    captions={}
+    params=list(confs.values())
+    captioners=list(confs.keys())
+
+    captions=multi_gpu_captioning(captioners,params,data)
+    with open(output,"w",encoding="utf-8") as output_file:
+        output_file.write(json.dumps(captions,indent=4,ensure_ascii=False))
