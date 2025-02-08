507688d8dc2fd6915684857b39b0577e405aa884
James Gore <73591234+jgore077@users.noreply.github.com>
2025-02-07 23:56:52 -0500

    add brief comments describing return types

--- src/calculate_captioning_metrics.py
+++ src/calculate_captioning_metrics.py
@@ -5,8 +5,12 @@ parser=CaptionMetricParser()
 args=parser.parse_args()
 
 metrics=[
+    # Returns p (precision)
     BleuMetric(),
+    # Return a METEOR scores
     MeteorMetric(),
+    # Returns averages of averages (p, r, f1)   
     RougeMetric(),
+    # Returns r's,r's,f1's
     BERTScoreMetric("distilbert-base-uncased")
 ]
