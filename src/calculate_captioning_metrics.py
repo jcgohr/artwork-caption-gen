5b911c332a5d8095577f740bb294c1d6fb998b44
James Gore <73591234+jgore077@users.noreply.github.com>
2025-02-10 16:18:11 -0500

    rearrange metrics for testing and fix bert score f1

--- src/calculate_captioning_metrics.py
+++ src/calculate_captioning_metrics.py
@@ -4,17 +4,21 @@ from parsers import CaptionMetricParser
 parser=CaptionMetricParser()
 parser.parse_args()
 
-scores={}
+
 metrics=[
-    # Returns p (precision)
-    (BleuMetric(),["bleu_precision"]),
-    # Return a METEOR scores
-    (MeteorMetric(),["meteor_score"]),
     # Returns averages of averages (p, r, f1)   
     (RougeMetric(),[["rouge-1-p","rouge-1-r","rouge-1-f1"],["rouge-2-p","rouge-2-r","rouge-2-f1"],["rouge-l-p","rouge-l-r","rouge-l-f1"]]),
     # Returns p's, r's, f1's
     (BERTScoreMetric("distilbert-base-uncased"),["distilbert-base-uncased-bertscore-p","distilbert-base-uncased-bertscore-r","distilbert-base-uncased-bertscore-f1"]),
+    # Returns p (precision)
+    (BleuMetric(),["bleu_precision"]),
+    # Return a METEOR scores
+    (MeteorMetric(),["meteor_score"]),
 ]
 
-for metric,keys in metrics:
+caption_keys=list(parser.data.keys())
+scores={k:{} for k in caption_keys}
+for metric,metric_keys in metrics:
     results=metric([captions[parser.key] for captions in list(parser.data.values())],[captions["True"] for captions in list(parser.data.values())])
+    assert len(caption_keys)==len(results),"Mismatch in key and results length"
+    print(results)
--- src/metrics/BERTScoreMetric.py
+++ src/metrics/BERTScoreMetric.py
@@ -1,12 +1,14 @@
 from bert_score import BERTScorer
+import torch
 
 class BERTScoreMetric():
     def __init__(self,model:str):
-        self.scorer=BERTScorer(model_type=model,lang="en")   
+        self.device="cuda" if torch.cuda.is_available() else "cpu"
+        self.scorer=BERTScorer(model_type=model,lang="en",device=self.device)   
     
     def __call__(self,candidate,reference):
         P, R, F1 = self.scorer.score(candidate, reference)
-        return [p.item() for p in P],[r.item() for r in R],[f1.item for f1 in F1]
+        return [p.item() for p in P],[r.item() for r in R],[f1.item() for f1 in F1]
     
 if __name__=="__main__":
     scr=BERTScoreMetric("distilbert-base-uncased")
