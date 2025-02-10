372e4e5ff3c13abe56ea72fc30316690b8d8b28f
James Gore <73591234+jgore077@users.noreply.github.com>
2025-02-10 13:42:55 -0500

    update bleu metric

--- src/metrics/BleuMetric.py
+++ src/metrics/BleuMetric.py
@@ -1,10 +1,21 @@
-from bleuscore import compute
+from torcheval.metrics import BLEUScore
+import torch
 
-class BleuMetric():    
+class BleuMetric():
+    def __init__(self):
+        self.bleu=BLEUScore(n_gram=4)
+
     def __call__(self,candidate,reference):
-        results = compute(predictions=candidate, references=reference)
-        return results
+        if type(candidate)==str and type(reference)==str:
+            self.bleu.update(candidate, reference)
+            return self.bleu.compute()
+        bleus=[]
+        for cand,ref in zip(candidate,reference):
+            self.bleu.reset()
+            self.bleu.update([cand],[ref])
+            bleus.append(self.bleu.compute().item())
+        return bleus
     
 if __name__=="__main__":
     bleu=BleuMetric()
-    bleu(["test test test","test test zee"],["test test test","test test test"])
+    print(bleu(["test test test test2","test test test zzee test3 zzee","test test test test4"],["test test test test2","test test test zzee test55 zzee","test test test test test44"]))
