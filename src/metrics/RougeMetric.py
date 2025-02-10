e24bdec59b7754e477f118734a8dada4011c6767
James Gore <73591234+jgore077@users.noreply.github.com>
2025-02-10 12:20:57 -0500

    modfiy rouge metric to return a floats instead of dicts

--- src/metrics/RougeMetric.py
+++ src/metrics/RougeMetric.py
@@ -5,4 +5,10 @@ class RougeMetric():
         self.rouge=Rouge()
         
     def __call__(self,candidate,reference):
-        return self.rouge.get_scores(candidate,reference)
+        return [list([list(dict(r).values()) for r in rouge_dict.values()]) for rouge_dict in self.rouge.get_scores(candidate,reference)]
+    
+if __name__=="__main__":
+    rge=RougeMetric()
+    # print(rge("test test test","test test test"))
+    print(rge(["test test test","test test zee"],["test test test","test test test"]))
+    
