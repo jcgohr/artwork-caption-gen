361e04b9f561487e861d394752cdd397dda16c4d
James Gore <73591234+jgore077@users.noreply.github.com>
2025-02-10 12:32:55 -0500

    modify keys for rouge

--- src/calculate_captioning_metrics.py
+++ src/calculate_captioning_metrics.py
@@ -11,7 +11,7 @@ metrics=[
     # Return a METEOR scores
     (MeteorMetric(),["meteor_score"]),
     # Returns averages of averages (p, r, f1)   
-    (RougeMetric(),["rouge-1-p","rouge-1-r","rouge-1-f1","rouge-2-p","rouge-2-r","rouge-2-f1","rouge-l-p","rouge-l-r","rouge-l-f1",]),
+    (RougeMetric(),[["rouge-1-p","rouge-1-r","rouge-1-f1"],["rouge-2-p","rouge-2-r","rouge-2-f1"],["rouge-l-p","rouge-l-r","rouge-l-f1"]]),
     # Returns p's, r's, f1's
     (BERTScoreMetric("distilbert-base-uncased"),["distilbert-base-uncased-bertscore"]),
 ]
