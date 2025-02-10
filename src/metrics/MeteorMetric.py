b5013843d4fad48a10f939d28945f85b51595173
James Gore <73591234+jgore077@users.noreply.github.com>
2025-02-10 11:25:08 -0500

    make meteor metric work on lists

--- src/metrics/MeteorMetric.py
+++ src/metrics/MeteorMetric.py
@@ -1,9 +1,12 @@
 from pymeteor.pymeteor import meteor as pymeteor
 
 class MeteorMetric():
-    def __init__(self):
-        pass
-    
     def __call__(self,candidate,reference):
-        return pymeteor(reference,candidate)
-
+        if type(candidate)==str and type(reference)==str:
+            return pymeteor(reference,candidate)
+        return [pymeteor(ref,cand) for ref,cand in zip(reference,candidate)]
+    
+if __name__=="__main__":
+    mtr=MeteorMetric()
+    print(mtr("test test test","test test test"))
+    print(mtr(["test test test","test test test"],["test test test","test test test"]))
