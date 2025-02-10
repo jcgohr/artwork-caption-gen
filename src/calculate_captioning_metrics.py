f0cbe8778f4af679e28a78863bff92f6f7eeae89
James Gore <73591234+jgore077@users.noreply.github.com>
2025-02-10 11:16:35 -0500

    refactor metrics to be tuples of metric objects and return keys

--- src/calculate_captioning_metrics.py
+++ src/calculate_captioning_metrics.py
@@ -4,13 +4,14 @@ from parsers import CaptionMetricParser
 parser=CaptionMetricParser()
 args=parser.parse_args()
 
+scores={}
 metrics=[
     # Returns p (precision)
-    BleuMetric(),
+    (BleuMetric(),["bleu_precision"]),
     # Return a METEOR scores
-    MeteorMetric(),
+    (MeteorMetric(),["meteor_score"]),
     # Returns averages of averages (p, r, f1)   
-    RougeMetric(),
-    # Returns r's,r's,f1's
-    BERTScoreMetric("distilbert-base-uncased")
+    (RougeMetric(),["rouge-1-p","rouge-1-r","rouge-1-f1","rouge-2-p","rouge-2-r","rouge-2-f1","rouge-l-p","rouge-l-r","rouge-l-f1",]),
+    # Returns p's, r's, f1's
+    (BERTScoreMetric("distilbert-base-uncased"),["distilbert-base-uncased-bertscore"]),
 ]
